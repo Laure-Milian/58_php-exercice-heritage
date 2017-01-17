900fad36a1f8d26c9e64d0c836427c71f5b2492d
Laure <laure.milian@gmail.com>
2017-01-17 10:43:57 +0100

    modifier des propriétés dans le constructeur de la class enfant

--- lib/MyCar.php
+++ lib/MyCar.php
@@ -1,5 +1,19 @@
 <?php
 
 class MyCar extends Car {
+
+	private $manufacturer;
+	private $kilometrage;
+	private $airbag;
+	private $abs;
+	private $doors;
+
+	public function __construct() {
+		parent::setManufacturer("Ford");
+		parent::setKilometrage("Beaucoup");
+		parent::setAirbag(true);
+		parent::setAbs(true);
+		parent::setDoors(4);
+	}
 	
 }
